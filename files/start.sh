1f948ce43b0f77bf41311a9c2df4c27211d81470
crocandr <slezsak.andras@gmail.com>
2026-01-31 21:14:50 +0100

    some message update

--- files/start.sh
+++ files/start.sh
@@ -2,12 +2,12 @@
 
 if [ ! -e /etc/minidlna.configured ]
 then
-  echo "Configuring minidlna ..."
+  echo "INFO: Configuring minidlna ..."
 
   # set friendly name
   sed -i "s@.*friendly_name=.*@friendly_name=$SRVNAME@g" /etc/minidlna.conf
   # set port
-  [ -z $PORT ] && { PORT=8201; }
+  [ -z $PORT ] && { PORT=8201; echo "INFO: port is undefined, using a predefined port: 8201"; }
   sed -i "s@^port=.*@port=$PORT@g" /etc/minidlna.conf
 
   # remove media folders
@@ -24,7 +24,7 @@ then
   # create configured flag file
   date > /etc/minidlna.configured
 else
-  echo "Using already exists config (/etc/minidlna.conf), starting minidlna ..."
+  echo "INFO: Using already exists config (/etc/minidlna.conf), starting minidlna ..."
 fi
 
 # minissdp
@@ -34,15 +34,15 @@ then
   SSDP_INTERFACE="$( ip addr | grep -i up | egrep -iv '(lo|br-|veth|docker).*:' | cut -f2 -d':' | tr -d ' ' )"
   if [ $( echo "$INTERFACE" | wc -l ) -gt 1 ]
   then
-    echo -e "You have more than one interface, choose one!:\n$SSDP_INTERFACE"
+    echo -e "ERROR: You have more than one interface, choose one!:\n$SSDP_INTERFACE"
     exit 1
   fi
   SSDP_IFACE=$SSDP_INTERFACE
 fi
 if [ $( ss -lntpu | grep -i 1900 | wc -l ) -gt 0 ]
 then
-  echo "Warning, another UPNP service use same UDP port!"
-  echo " Please stop another server and try again."
+  echo "WARNING: another UPNP service use same UDP port!"
+  echo "  Please stop another server and try again."
 fi
 minissdpd -i $SSDP_IFACE || { echo "SSDP service start problem"; exit 1; }
 
